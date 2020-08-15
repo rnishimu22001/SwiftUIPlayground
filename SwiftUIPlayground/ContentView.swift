0516f87195ca6d6a29dcf047def7d661f46ec7f5
rnishimu22001 <rnishimu22001@yahoo.co.jp>
2020-08-15 13:11:47 +0900

    Initial Commit

---
+++ SwiftUIPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPlayground
+//
+//  Created by rnishimu on 2020/08/15.
+//  Copyright © 2020 rnishimu22001. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
